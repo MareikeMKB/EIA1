503086d353892bc0ef8c6af0a48f51f330a4ff38
MareikeMKB <mareike.bitz@gmx.net>
2022-11-24 15:28:10 +0100

    neueAufgabe

--- JavaScript/script.ts
+++ JavaScript/script.ts
@@ -1,4 +1,4 @@
-const einwohnerf = 65617812;
+/*const einwohnerf = 65617812;
 const einwohnereu = 447010000;
 const einwohnerfp = 62879530;
 const prozentf = einwohnerf * 100 / einwohnereu;
@@ -45,4 +45,4 @@ console.log ('SCHWEDEN')
 console.log('Schweden hat im Jahr 2022 ' + einwohners + ' Einwohner.');
 console.log('Das sind ' + prozents + '% der Einwohner der EU.');
 console.log('Im Vergleich zu 2010 sind das ' + prozentsp + '% mehr Einwohner.');
-console.log('Das ist eine Differenz von ' + einwohnersd + ' Einwohnern.');
+console.log('Das ist eine Differenz von ' + einwohnersd + ' Einwohnern.');*/
---
+++ JavaScriptHTML/script.js
@@ -0,0 +1,49 @@
+const einwohnerf = 65617812;
+const einwohnereu = 447010000;
+const einwohnerfp = 62879530;
+const prozentf = einwohnerf * 100 / einwohnereu;
+const prozentfp = (einwohnerf - einwohnerfp) * 100 / einwohnerfp;
+const einwohnerfd = einwohnerf - einwohnerfp;
+const einwohnerd = 84424123;
+const einwohnerdp = 80827002;
+const prozentd = einwohnerd * 100 / einwohnereu;
+const prozentdp = (einwohnerd - einwohnerdp) * 100 / einwohnerdp;
+const einwohnerdd = einwohnerd - einwohnerdp;
+const einwohneri = 60251810;
+const einwohnerip = 59325229;
+const prozenti = einwohneri * 100 / einwohnereu;
+const prozentip = (einwohneri - einwohnerip) * 100 / einwohnerip;
+const einwohnerid = einwohneri - einwohnerip;
+const einwohners = 10250905;
+const einwohnersp = 9390168;
+const prozents = einwohners * 100 / einwohnereu;
+const prozentsp = (einwohners - einwohnersp) * 100 / einwohnersp;
+const einwohnersd = einwohners - einwohnersp;
+function setNumbers(land) {
+}
+/* Funktion mit Daten (Land) und mit Variablen Funktion für Ausgabe machen, die je nach angeklicktem Land (eingegebene Daten) die Ausgabe anpasst, dass man nicht 5000 Funktionen hat, die das exakt selbe machen - oder mit EventListener
+Anonyme Funktion (function ()) über Event-Listener und erst drinnen tatsächliche Funktion aufrufen*/
+/* console.log ('FRANKREICH')
+console.log('Frankreich hat im Jahr 2022 ' + einwohnerf + ' Einwohner.');
+console.log('Das sind ' + prozentf + '% der Einwohner der EU.');
+console.log('Im Vergleich zu 2010 sind das ' + prozentfp + '% mehr Einwohner.');
+console.log('Das ist eine Differenz von ' + einwohnerfd + ' Einwohnern.');
+
+console.log ('DEUTSCHLAND')
+console.log('Deutschland hat im Jahr 2022 ' + einwohnerd + ' Einwohner.');
+console.log('Das sind ' + prozentd + '% der Einwohner der EU.');
+console.log('Im Vergleich zu 2010 sind das ' + prozentdp + '% mehr Einwohner.');
+console.log('Das ist eine Differenz von ' + einwohnerdd + ' Einwohnern.');
+
+console.log ('ITALIEN')
+console.log('Italien hat im Jahr 2022 ' + einwohneri + ' Einwohner.');
+console.log('Das sind ' + prozenti + '% der Einwohner der EU.');
+console.log('Im Vergleich zu 2010 sind das ' + prozentip + '% mehr Einwohner.');
+console.log('Das ist eine Differenz von ' + einwohnerid + ' Einwohnern.');
+
+console.log ('SCHWEDEN')
+console.log('Schweden hat im Jahr 2022 ' + einwohners + ' Einwohner.');
+console.log('Das sind ' + prozents + '% der Einwohner der EU.');
+console.log('Im Vergleich zu 2010 sind das ' + prozentsp + '% mehr Einwohner.');
+console.log('Das ist eine Differenz von ' + einwohnersd + ' Einwohnern.'); */
+//# sourceMappingURL=script.js.map
