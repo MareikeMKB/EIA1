249605ed8117b67bbe43be155725b7cce755447d
MareikeMKB <mareike.bitz@gmx.net>
2022-12-01 16:26:55 +0100

    aufgabe8

---
+++ DrumPad/script.ts
@@ -0,0 +1,78 @@
+const soundh:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/hihat.mp3');
+const soundk:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/kick.mp3');
+const sounds:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/snare.mp3');
+const sounda:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/A.mp3');
+const soundc:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/C.mp3');
+const soundf:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/F.mp3');
+const soundg:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/G.mp3');
+const soundl1:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/laugh-1.mp3');
+const soundl2:HTMLAudioElement = new Audio('/L08/task_material/assets/DrumPad/laugh-2.mp3');
+
+let song: any [] = [];
+
+
+document.querySelector('#h').addEventListener('click', function() {
+    soundh.play();
+    song.push(soundh);
+}
+)
+
+document.querySelector('#k').addEventListener('click', function() {
+    soundk.play();
+    song.push(soundk);
+}
+)
+
+document.querySelector('#s').addEventListener('click', function() {
+    sounds.play();
+    song.push(sounds);
+}
+) 
+
+document.querySelector('#a').addEventListener('click', function() {
+    sounda.play();
+    song.push(sounda);
+}
+)
+
+document.querySelector('#c').addEventListener('click', function() {
+    soundc.play();
+    song.push(soundc);
+}
+)
+
+document.querySelector('#f').addEventListener('click', function() {
+    soundf.play();
+    song.push(soundf);
+}
+)
+
+document.querySelector('#g').addEventListener('click', function() {
+    soundg.play();
+    song.push(soundg);
+}
+)
+
+
+document.querySelector('#l1').addEventListener('click', function() {
+    soundl1.play();
+    song.push(soundl1);
+}
+)
+
+document.querySelector('#l2').addEventListener('click', function() {
+    soundl2.play();
+    song.push(soundl2);
+}
+)
+
+
+let counter = 0;
+
+document.querySelector('#image').addEventListener('click', function() {
+    setInterval(function() {
+        song[counter].play();
+        counter++
+    }, 700);
+}
+)
